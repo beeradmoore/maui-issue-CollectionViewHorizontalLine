5acdbc63a8d947dc008c447ea6cbb8992a080beb
Brad <brad@4pi.com.au>
2024-12-16 13:59:04 +1100

    Initial commit

---
+++ MauiCollectionViewLineIssue/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiCollectionViewLineIssue;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        // Change this to go directly to the test page to see the issue go away
+        //return new Window(new TestPage());
+        return new Window(new MainPage());
+    }
+}
---
+++ MauiCollectionViewLineIssue/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiCollectionViewLineIssue;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    void Button_OnClicked(object? sender, EventArgs e)
+    {
+        App.Current.Windows[0].Page = new TestPage();
+    }
+}
